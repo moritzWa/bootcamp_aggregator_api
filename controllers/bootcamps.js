3b140eb8bc8e8628631d343afccff1ca1e694d8a
Moritz Wallawitsch <wallawitsch@gmail.com>
2020-01-25 09:09:29 +0100

    pagination: next and prev

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,25 +10,82 @@ const ErrorResponse = require("../utils/errorResponse.js")
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  let query
+
   // bootcamps?location.state=MA&housing=true
   // { 'location.state': 'MA', housing: 'true' }
 
   //bootcamps?averageCost[lte]=10000
   // { averageCost: { lte: '10000' } }
 
-  let query
-  let queryStr = JSON.stringify(req.query)
+  // Copy req.query
+  const reqQurey = { ...req.query }
 
+  // Fields to exclude
+  const removeFields = ["select", "sort", "page", "limit"]
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQurey[param])
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQurey)
+
+  // Add $ to Comparison Query Operators
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
+  // Find resource
   query = Bootcamp.find(JSON.parse(queryStr))
-  console.log(req.query, " => ", queryStr)
 
+  //Select Fieds / format like query(field1 field2)
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ")
+    query = query.select(fields)
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ")
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort("-createdAt")
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 1
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+  const total = await Bootcamp.countDocuments()
+
+  query = query.skip(startIndex).limit(limit)
+
+  // Executing query
   const bootcamps = await query
 
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps })
+  // Pagination result
+  //  show next&previous only when available
+  const pagination = {}
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps
+  })
 })
 
 // @desc      Get single bootcamp
