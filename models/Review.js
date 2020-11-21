22823e493a4a7b5c194087817b1958e14067a655
Moritz Wallawitsch <wallawitsch@gmail.com>
2020-11-21 16:14:31 +0100

    Get Create Review and Review Seeder

--- models/Review.js
+++ models/Review.js
@@ -33,4 +33,8 @@ const ReviewSchema = new mongoose.Schema({
   }
 });
 
+// Prevent from submitting more than one review per bootcamp
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+
+
 module.exports = mongoose.model('Review', ReviewSchema);
